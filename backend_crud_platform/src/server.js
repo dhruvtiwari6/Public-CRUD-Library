da48603ff64a02bd32c6e38d212d7f4b1ba0c3c3
dhruvtiwari6 <144548881+dhruvtiwari6@users.noreply.github.com>
2025-04-21 21:05:36 +0530

    testing

--- backend_crud_platform/src/server.js
+++ backend_crud_platform/src/server.js
@@ -17,20 +17,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,  // Ensure this points to your secret key
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000,  // 1 day
-    }
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,  // Ensure this points to your secret key
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       maxAge: 24 * 60 * 60 * 1000,  // 1 day
+//     }
+//   })
+// );
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Backend is working!');
@@ -41,21 +41,21 @@ app.use("/api", crudRoutes);
 
 
 
-sequelize
-  .sync({ alter: true }) 
-  .then(() => {
-    console.log("Database synced");
+// sequelize
+//   .sync({ alter: true }) 
+//   .then(() => {
+//     console.log("Database synced");
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log("Server running on port", process.env.PORT || 5000);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Error syncing database:", err);
+//   });
+
+
     app.listen(process.env.PORT || 5000, () => {
       console.log("Server running on port", process.env.PORT || 5000);
     });
-  })
-  .catch((err) => {
-    console.error("Error syncing database:", err);
-  });
-
-
-    // app.listen(process.env.PORT || 5000, () => {
-    //   console.log("Server running on port", process.env.PORT || 5000);
-    // });
 
 
